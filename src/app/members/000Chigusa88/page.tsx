c4da6a02af3ef6e63e9c59eeb63da7ea8d7be3b8
{Chigusa} <{000.Chigusa88@gmail.com}>
2025-10-24 00:32:41 +0900

    Add 000Chigusa88 member page and profile image

---
+++ src/app/members/000Chigusa88/page.tsx
@@ -0,0 +1,13 @@
+red = "\033[31m"
+green = "\033[32m"
+yellow = "\033[33m"
+blue = "\033[34m"
+magenta = "\033[35m"
+cyan = "\033[36m"
+white = "\033[37m"
+
+print(red+"name：ちぐさ")
+
+print(blue+"一言：プログラミング初心者です")
+
+print(white)
